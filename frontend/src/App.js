3fd0ccf27f73dc9720626865c9954cd2b165928d
nilu nilesh <ajabdas999@gmail.com>
2020-08-28 19:48:07 +0530

    new version added

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Nav from './comp/home/Nav'
+import Home from './comp/home/Home'
+import {BrowserRouter as Router, Switch,Route,Redirect} from 'react-router-dom'
+import Chat from './comp/friend/Chat';
+import Login from './comp/login/Login';
+import { connect } from 'react-redux';
+import {checkLogin} from './redux/action/action'
+import Profile from './comp/Profile/Profile'
+
+import '../node_modules/antd/dist/antd.css'
+import withState from './comp/hoc/withState';
+function App(props) {
+  checkLogin()
+  return (
+    <Router>
+    <div className='true' style={{minWidth:"600px"}}>
+      {props.username!==null?<Nav/>:<Login/>}
+      <Switch>
+      <Route exact path='/' component={Home}/>
+        <Route exact path='/messages' component={Chat}/>
+        {props.userid!==null?
+        <Route exact path='/profile' component={Profile}/>:<Redirect to='/'/>}
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+const mapStateToProps = state =>{return {...state}}
+
+export default connect(mapStateToProps)(withState(App))
--- src/comp/home/backup/Badge.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Badge(props) {
-    return (
-    <span className="badge badge-danger">{props.data}</span>
-    )
-}
